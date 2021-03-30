2f500c349a12c024194bb597af74dc5fe5822cc2
Rob Greene <greener2@gmail.com>
2021-03-30 21:34:35 +0100

    Completed app with login, signup and logout functionality

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flask_auth_app
+
+Example app (based on Digital Ocean blogpost) to demonstrate the use of flask-login for authenticating users.
--- auth_app/__init__.py
+++ auth_app/__init__.py
@@ -1,6 +1,8 @@
 import os
 
 from flask import Flask
+from flask.cli import load_dotenv
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -10,10 +12,22 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
+    load_dotenv("../.flaskenv")
+
     app.config["SECRET_KEY"] = os.environ.get("FLASK_SECRET", "supersecretkey")
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
